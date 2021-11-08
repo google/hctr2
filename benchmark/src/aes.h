01a7371d9721d1e95de4ddf7abf6ee1e99e006db
Eric Biggers <ebiggers@google.com>
2021-11-08 12:38:44 -0800

    Remove (C) from copyright lines
    "(C)" is not recommended to include these days.

--- benchmark/src/aes.h
+++ benchmark/src/aes.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Google LLC
+ * Copyright 2018 Google LLC
  *
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
